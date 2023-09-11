6a0528a1af546d52ad4a0d9792fe12de028b5786
Manideep Innamuri <deepu.innamuri@gmail.com>
2023-09-11 17:42:39 +0530

    # Added Model and Context classes

---
+++ Shopping Application/ShoppingApplication.Common/ShoppingContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingApplication.Common.Models;
+
+namespace ShoppingApplication.Common
+{
+    public class ShoppingContext : DbContext
+    {
+        public ShoppingContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Product> MyProperty { get; set; }
+    }
+}
